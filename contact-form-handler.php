99a91c9e481be6178c812c3be6ff21d4280e0073
mah2uk <67327165+mah2uk@users.noreply.github.com>
2020-07-02 16:36:57 +0100

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -3,13 +3,15 @@
   $visitor_email = $_POST ['email'];
   $message = $_POST['message'];
 
+
   $email_from = 'contact@krafty-designs.com';
 
   $email_subject = "New Form Submission";
 
   $email_body = "User Name: $name.\n".
-                "User Email: $visitor_email.\n".
-                "User Message: $message.\n".;
+                  "User Email: $visitor_email.\n".
+                    "User Message: $message.\n".;
+
 
 $to = "admin@kraty-designs.com";
 
