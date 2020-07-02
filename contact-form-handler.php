f497c777cbbb3ec2126336f65fe06f8cbb9b31aa
mah2uk <67327165+mah2uk@users.noreply.github.com>
2020-07-02 16:32:16 +0100

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -11,7 +11,7 @@
                 "User Email: $visitor_email.\n".
                 "User Message: $message.\n".;
 
-$to = "admin@kraty-designs.com"
+$to = "admin@kraty-designs.com";
 
 $headers = "From: $email_from \r\n";
 
